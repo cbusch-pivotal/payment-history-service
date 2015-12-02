82933081cf63e29f4a05db063a190765474b84aa
Chris Busch <cbusch@pivotal.io>
2015-12-01 21:46:31 -0600

    Add test parameters for consumer message call

--- src/main/java/io/pivotal/mastercard/payment/history/ConsumerMessagingClient.java
+++ src/main/java/io/pivotal/mastercard/payment/history/ConsumerMessagingClient.java
@@ -31,15 +31,32 @@ public class ConsumerMessagingClient extends WebServiceGatewaySupport {
 						request);
 						
 		return response;
-		
-		
 	}
 	
 	public DeviceNotificationResponse getDeviceNotificationResponse() {
 		
 		DeviceNotificationRequest request = new DeviceNotificationRequest();
+		
+/*		<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.consumerMessaging.mastercard.com">
+		   <soapenv:Header/>
+		   <soapenv:Body>
+		      <ser:DeviceNotificationRequest>
+		         <ApplicationId>APP-01</ApplicationId>
+		         <DeviceToken>253ae4f9362a645b340f5d8d23f0db9659a3e9e470f5b18e4c506cff08cf0b3a</DeviceToken>
+		         <NotificationText>test message</NotificationText>
+		         <Apple>Y</Apple>
+		         <Google>N</Google>
+		         <CorrelationId/>
+		      </ser:DeviceNotificationRequest>
+		   </soapenv:Body>
+		</soapenv:Envelope>
+*/
+		      
+		// test load - update with method parameters to make it dynamic
 		request.setApplicationId("APP-01");
 		request.setDeviceToken("253ae4f9362a645b340f5d8d23f0db9659a3e9e470f5b18e4c506cff08cf0b3a");
+		request.setApple("Y");
+		request.setGoogle("N");
 		
 		DeviceNotificationResponse response = (DeviceNotificationResponse) 
 				this.getWebServiceTemplate().marshalSendAndReceive(
@@ -47,7 +64,5 @@ public class ConsumerMessagingClient extends WebServiceGatewaySupport {
 						request);
 		
 		return response;
-		
 	}
-
 }
